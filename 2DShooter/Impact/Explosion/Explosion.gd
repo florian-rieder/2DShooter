ab8207f8d613af15bbf733c7044c1473e7e69d19
Sergenti <florian.rieder@unil.ch>
2022-09-08 00:23:07 +0200

    Added shotgun

--- 2DShooter/Impact/Explosion/Explosion.gd
+++ 2DShooter/Impact/Explosion/Explosion.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
 
-export var damage = 500
+export var damage = 200
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    # wait .1 second in order to prevent an error with the Area2D
     yield(get_tree().create_timer(0.1), "timeout")
     var bodies = get_overlapping_bodies()
 
@@ -14,7 +15,7 @@ func _ready():
             var distance = body.global_position.distance_to(global_position)/16
             var effective_damage = damage/(distance*distance)
             print(String(distance) + "  " + String(effective_damage))
-            body.call('take_hit', damage)
+            body.call('take_hit', round(effective_damage))
     
     $AnimatedSprite.playing = true
 

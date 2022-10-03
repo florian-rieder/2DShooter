d110d3e01869293e0b3267740f94623cd35d1375
Sergenti <florian.rieder@unil.ch>
2022-10-03 12:27:24 +0200

    fixed health bar last stand

--- 2DShooter/UI/HealthBar/HealthBar.gd
+++ 2DShooter/UI/HealthBar/HealthBar.gd
@@ -1,3 +1,5 @@
+# simple health bar using ProgressTexture.
+# allows "last stand" mechanic: the last pixel has more health
 extends Node2D
 
 onready var healthbar = $Progress
@@ -5,28 +7,35 @@ onready var healthbar = $Progress
 var bar_green = preload("res://UI/HealthBar/Progress.png")
 var bar_red = preload("res://UI/HealthBar/ProgressRed.png")
 
+# percentage at which the healthbar turns red
 var red_percent_threshold = 0.4
+# health level at which the healthbar will display 1px
 export var last_stand_threshold = 0
+
 
 func _ready():
     hide()
 
 
 func set_max_value(max_value):
-    healthbar.max_value = max_value
+    healthbar.max_value = max_value - last_stand_threshold
 
 
 func update_bar(value):
-    if value >= healthbar.max_value:
+    var adjusted_value = value - last_stand_threshold
+    # hide the healthbar if full
+    if adjusted_value >= healthbar.max_value:
         hide()
         return
+    # change color in function of value
     healthbar.texture_progress = bar_green
-    if value < (healthbar.max_value) * red_percent_threshold:
+    if adjusted_value < healthbar.max_value * red_percent_threshold:
         healthbar.texture_progress = bar_red
-    if value < healthbar.max_value:
+    # show the healthbar if not full
+    if adjusted_value < healthbar.max_value:
         show()
-    
-    if value < last_stand_threshold:
-        healthbar.value = 5
+    # change the value
+    if adjusted_value < 1:
+        healthbar.value = 1
     else:
-        healthbar.value = value
+        healthbar.value = adjusted_value
